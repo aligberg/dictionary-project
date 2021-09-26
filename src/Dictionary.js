58b02041da43a19a3ce9f8284f2f09c4f2a89e60
Ali Greenberg <abgreenb2@gmail.com>
2021-09-26 13:26:08 -0400

    added Meaning component and loop through definitions and parts of speech

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results.js";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState(null);
-
+  const [results, setResults] = useState(null);
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
+
   }  
   function search(event) {
     event.preventDefault();
@@ -18,9 +21,12 @@ export default function Dictionary() {
   }
 
   return (
+  <div>
     <form className="text-center" onSubmit={search}>
       <input type="search" placeholder="Type a word..." onChange={handleKeyword}></input>
       <input type="submit" value="Search"></input>
     </form>
+    <Results results={results} />
+  </div>
   );
 }
